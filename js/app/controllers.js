97e9d939d45ff9ef3d1c4133cbfb29290aa0d733
Joshua Sera <me@joshsera.com>
2016-04-11 20:25:40 -0700

    Frontend for adding and deleting pictures works

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -7,6 +7,9 @@ angular.module("FishCtrls", [])
 
     $scope.ids = {};
 
+    $scope.newPic = {};
+    $scope.newPicDisabled = false;
+
     $scope.editNames = function($event) {
         $event.preventDefault();
 
@@ -34,5 +37,45 @@ angular.module("FishCtrls", [])
 
     $scope.deletePic = function($event, pic) {
         $event.preventDefault();
+        pic.btnDisabled = true;
+        var req = {
+            url: "/api/pictures/"+pic.id,
+            method: "DELETE"
+        };
+
+        $http(req).then(function(res) {
+            pic.btnDisabled = false;
+            if (res.status === 200) {
+                $scope.fish.fishpics.splice($scope.fish.fishpics.indexOf(pic), 1);
+                console.log("Deleted?");
+            } else {
+                console.log("Problem deleting pic: ", res);
+            }
+        });
+    };
+
+    $scope.addPic = function($event) {
+        $event.preventDefault();
+        $scope.newPicDisabled = true;
+
+        $scope.newPic.fishId = $scope.fish.id;
+        $scope.newPic.userId = $scope.ids.userId;
+
+        var req = {
+            url: "/api/pictures/",
+            method: "POST",
+            data: $scope.newPic
+        };
+
+        $http(req).then(function(res) {
+            $scope.newPicDisabled = false;
+            if (res.status === 200) {
+                res.data.btnDisabled = false;
+                $scope.fish.fishpics.push(res.data);
+                console.log("New pic: "+res.data);
+            } else {
+                console.log("Problem creating new pic: ", res);
+            }
+        });
     };
 }]);
