88d9c0de71862bdd093829be855d6c3f8afa803c
Joshua Sera <me@joshsera.com>
2016-04-13 15:09:27 -0700

    Searching for a fish in the fish widget works

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -111,10 +111,20 @@ angular.module("FishCtrls", ["ngAnimate", "ui.bootstrap"])
 
 .controller("FishFindCtrl", ["$scope", "$http", function($scope, $http) {
     $scope.commonname = "";
+    $scope.binomialname = "";
     $scope.loadingFish = false;
     $scope.noResults = false;
     $scope.loadingBinomial = false;
     $scope.noBinomialResults = false;
+    $scope.noFishFound = false;
+    $scope.fishFound = false;
+    $scope.fish = {};
+
+    var formatNames = function(fish) {
+        fish.binomialName = fish.genus.name + " " + fish.species;
+        fish.formattedCommonNames = fish.commonnames.split(",").join(", ");
+        return fish;
+    };
 
     $scope.autocompleteCommonName = function(query) {
         var req = {
@@ -158,9 +168,49 @@ angular.module("FishCtrls", ["ngAnimate", "ui.bootstrap"])
 
     $scope.searchByCommonName = function($event) {
         $event.preventDefault();
+        $scope.fish = null;
+        $scope.fishFound = false;
+        var req = {
+            method: "GET",
+            url: "/api/fish",
+            params: {
+                commonname: $scope.commonname
+            }
+        };
+
+        $http(req).then(function(res) {
+            if (res.data.results && res.data.results.join) {
+                $scope.fishFound = !($scope.noFishFound = false);
+                $scope.fish = formatNames(res.data.results[0]);
+            } else {
+                $scope.fishFound = !($scope.noFishFound = true);
+            }
+        }, function(res) {
+            $scope.fishFound = !($scope.noFishFound = true);
+        });
     };
 
     $scope.searchByBinomial = function($event) {
         $event.preventDefault();
+        $scope.fish = null;
+        $scope.fishFound = false;
+        var req = {
+            method: "GET",
+            url: "/api/fish",
+            params: {
+                scientificname: $scope.binomialname
+            }
+        };
+
+        $http(req).then(function(res) {
+            if (res.data.results && res.data.results.join) {
+                $scope.fishFound = !($scope.noFishFound = false);
+                $scope.fish = formatNames(res.data.results[0]);
+            } else {
+                $scope.fishFound = !($scope.noFishFound = true);
+            }
+        }, function(res) {
+            $scope.fishFound = !($scope.noFishFound = true);
+        });
     };
 }]);
