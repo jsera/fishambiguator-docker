387f651372370a191c371942b01454e0e69cee5e
Joshua Sera <me@joshsera.com>
2016-07-06 20:31:46 -0700

    Adding some comments for future plans

--- js/app/directives/comment-editor.js
+++ js/app/directives/comment-editor.js
@@ -1,7 +1,9 @@
 angular.module("CommentEditor", [])
 
 .controller("CommentEditCtrl", ["$scope", "$http", function($scope, $http) {
-
+    // Just load all comments for this fish pair. The backend will take care of reciprocal pairs
+    // If we get comments, display them. All comments by this editor are editable.
+    // No comments means the pair doesn't exist. Just display a new comment form
 }])
 
 .directive("commentEditor", function() {
